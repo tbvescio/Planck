827a41ab7277f8bcbc7de4ee274423cb1f0c2215
tomas <tbvescio@gmail.com>
2021-05-23 19:43:43 -0300

    add docker-compose

--- script.sql
+++ script.sql
@@ -1,12 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.8.5
--- https://www.phpmyadmin.net/
---
--- Host: localhost
--- Generation Time: Sep 01, 2020 at 02:44 PM
--- Server version: 8.0.13-4
--- PHP Version: 7.2.24-0ubuntu0.18.04.6
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
@@ -18,9 +9,6 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `6mcn7ar5DY`
---
 
 -- --------------------------------------------------------
 
@@ -32,11 +20,9 @@ CREATE TABLE `clicks_log` (
   `idCLICKS_LOG` int(11) NOT NULL,
   `browser` varchar(45) DEFAULT NULL,
   `os` varchar(45) DEFAULT NULL,
-  `idUrl` int(11) DEFAULT NULL
+  `idUrl` int(11) DEFAULT NULL,
+  `inserted_at` date NOT NULL DEFAULT '1970-01-01'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
--- --------------------------------------------------------
 
 --
 -- Table structure for table `urls`
@@ -46,9 +32,9 @@ CREATE TABLE `urls` (
   `idURLs` int(11) NOT NULL,
   `url` varchar(255) DEFAULT NULL,
   `urlConverted` varchar(50) DEFAULT NULL,
-  `idUser` int(11) DEFAULT NULL
+  `idUser` int(11) DEFAULT NULL,
+  `createdAt` date NOT NULL DEFAULT '1970-01-01'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 
 -- --------------------------------------------------------
 
@@ -61,13 +47,6 @@ CREATE TABLE `users` (
   `username` varchar(45) DEFAULT NULL,
   `password` varchar(150) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`idUSERS`, `username`, `password`) VALUES
-(1, 'test', '$2b$12$CThGAYx/W44r93OwjBLj2.D2ODTASsQDROwoyUXcLeaivcZMMVEya');
 
 --
 -- Indexes for dumped tables
@@ -101,20 +80,17 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `clicks_log`
 --
 ALTER TABLE `clicks_log`
-  MODIFY `idCLICKS_LOG` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
+  MODIFY `idCLICKS_LOG` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=84;
 --
 -- AUTO_INCREMENT for table `urls`
 --
 ALTER TABLE `urls`
-  MODIFY `idURLs` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-
+  MODIFY `idURLs` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `idUSERS` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
+  MODIFY `idUSERS` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 --
 -- Constraints for dumped tables
 --
